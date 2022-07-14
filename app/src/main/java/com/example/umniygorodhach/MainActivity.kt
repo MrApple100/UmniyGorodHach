35108109920b8a86feae23112dbe003b84b811e5
Alexander <mrapple100@yandex.ru>
2022-07-14 18:19:24 +0300

    MainActivity settings

--- app/src/main/java/com/example/umniygorodhach/MainActivity.kt
+++ app/src/main/java/com/example/umniygorodhach/MainActivity.kt
@@ -1,43 +1,55 @@
 package com.example.umniygorodhach
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.ExperimentalTransitionApi
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.umniygorodhach.ui.theme.UmniyGorodHachTheme
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.navigation.compose.rememberNavController
+import com.example.umniygorodhach.presentation.ui.ITLabApp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import kotlinx.serialization.ExperimentalSerializationApi
+import com.example.umniygorodhach.presentation.navigation.LocalNavController
+import com.example.umniygorodhach.presentation.ui.theme.ITLabTheme
+import com.example.umniygorodhach.presentation.utils.LocalActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalSerializationApi
+@ExperimentalPagerApi
+@ExperimentalAnimationApi
+@ExperimentalTransitionApi
+@ExperimentalMaterialApi
+@ExperimentalMotionApi
+@ExperimentalStdlibApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UmniyGorodHachTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+           // val authState by authViewModel.authStateFlow.collectAsState(null)
+            ITLabTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    //when (authState?.isAuthorized) {
+                     //   true -> {
+                            CompositionLocalProvider(
+                                LocalNavController provides rememberNavController(),
+                                LocalActivity provides this
+                            ) {
+                                ITLabApp()
+                            }
+                        //}
+                        //false -> AuthScreen { authViewModel.onLoginEvent(authPageLauncher) }
+                       // null -> {}
+                   // }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    UmniyGorodHachTheme {
-        Greeting("Android")
-    }
-}
