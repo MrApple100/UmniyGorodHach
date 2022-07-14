ad19ebbcfa45041b6a1832359f4aac352c1be7b3
Alexander <mrapple100@yandex.ru>
2022-07-14 19:25:43 +0300

    Top_App_Bar

---
+++ app/src/main/java/com/example/umniygorodhach/presentation/ui/components/top_app_bars/AppTabsViewModel.kt
@@ -0,0 +1,80 @@
+package com.example.umniygorodhach.presentation.ui.components.top_app_bars
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import com.example.umniygorodhach.presentation.utils.AppTab
+import javax.inject.Inject
+
+@HiltViewModel
+class AppTabsViewModel @Inject constructor(
+   // authStateStorage: AuthStateStorage
+) : ViewModel() {
+   // private val userClaimsFlow = authStateStorage.userClaimsFlow
+
+    private val _statePage = MutableStateFlow(1)
+    val statePage = _statePage.asStateFlow()
+
+    private val _pagesSize = MutableStateFlow(intArrayOf(5, 2))
+    val pagesSize = _pagesSize.asStateFlow()
+
+    private val _appTabs = MutableStateFlow(AppTab.all)
+    val appTabs = _appTabs.asStateFlow()
+
+
+    init {
+        viewModelScope.launch {
+           // userClaimsFlow.collect {
+               // AppTab.applyClaims(it)
+
+                var from = 0
+                val appTabsAccess = allAppTabsAccess()
+
+                var several = 1
+
+                for (i in 0 until pagesSize.value.size) {
+                    from += pagesSize.value[i]
+                    if (from >= appTabsAccess.size) {
+                        if (several < i + 1)
+                            several = i + 1
+                    }
+                }
+                _pagesSize.value = _pagesSize.value.copyOfRange(0, several)
+                _appTabs.value = AppTab.all.toList()
+           // }
+        }
+    }
+
+    fun changePage(pagesSize: IntArray, number: Int): List<AppTab> {
+        var numberPage = (number + pagesSize.size - 1) % pagesSize.size
+        _statePage.value = numberPage + 1
+
+        var from = 0
+
+        for (i in 0 until numberPage) {
+            from += pagesSize[i]
+
+        }
+        val appTabsAccess = allAppTabsAccess()
+
+        if (from >= appTabsAccess.size) {
+            from = 0
+            numberPage = 0
+            _statePage.value = numberPage + 1
+        }
+        if (from + pagesSize[numberPage] > appTabsAccess.size) {
+            return appTabsAccess.subList(from, appTabsAccess.size)
+
+        } else {
+            return appTabsAccess.subList(from, from + pagesSize[numberPage])
+        }
+
+    }
+
+    fun allAppTabsAccess(): List<AppTab> {
+        return _appTabs.value.filter { it.accessible }
+    }
+}
---
+++ app/src/main/java/com/example/umniygorodhach/presentation/ui/components/top_app_bars/TopAppBar.kt
@@ -0,0 +1,291 @@
+package com.example.umniygorodhach.presentation.ui.components.top_app_bars
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.umniygorodhach.presentation.ui.components.AppDropdownMenu
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import kotlinx.coroutines.launch
+import com.example.umniygorodhach.presentation.ui.theme.AppColors
+import com.example.umniygorodhach.presentation.utils.AppBarTab
+import java.util.*
+
+@Composable
+fun BasicTopAppBar(
+    text: String,
+    onBackAction: () -> Unit = emptyBackAction,
+    options: List<AppBarOption> = emptyList()
+) {
+	TopAppBar {
+		Row(
+			modifier = Modifier
+				.fillMaxWidth()
+				.height(56.dp)
+				.padding(
+					start = if (onBackAction == emptyBackAction) 16.dp else 0.dp,
+					end = 16.dp
+				),
+			verticalAlignment = Alignment.CenterVertically,
+			horizontalArrangement = Arrangement.SpaceBetween
+		) {
+			Row(
+				horizontalArrangement = Arrangement.Start,
+				verticalAlignment = Alignment.CenterVertically
+			) {
+				if (onBackAction != emptyBackAction) {
+					IconButton(onClick = onBackAction) {
+						Icon(Icons.Default.ArrowBack, contentDescription = null)
+					}
+					Spacer(modifier = Modifier.width(16.dp))
+				}
+
+				Text(
+					text = text,
+					fontSize = 20.sp,
+					fontWeight = FontWeight(500),
+					textAlign = TextAlign.Start,
+					color = MaterialTheme.colors.onSurface,
+					maxLines = 1,
+					overflow = TextOverflow.Ellipsis
+				)
+			}
+
+			OptionsRow(options)
+		}
+	}
+}
+
+@Composable
+fun ExtendedTopAppBar(
+    options: List<AppBarOption> = emptyList(),
+    onBackAction: () -> Unit = emptyBackAction,
+    hideBackButton: Boolean = false,
+    hideOptions: Boolean = false,
+    content: @Composable () -> Unit
+) {
+	TopAppBar {
+		ExtendedTopAppBarBody(
+			options, onBackAction, hideBackButton, hideOptions, content
+		)
+	}
+}
+
+@Composable
+fun ExtendedTopAppBarBody(
+    options: List<AppBarOption> = emptyList(),
+    onBackAction: () -> Unit = emptyBackAction,
+    hideBackButton: Boolean = false,
+    hideOptions: Boolean = false,
+    content: @Composable () -> Unit
+) {
+	Row(
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(
+				start = if (hideBackButton) 16.dp else 0.dp,
+				end = 16.dp
+			),
+		verticalAlignment = Alignment.CenterVertically,
+		horizontalArrangement = Arrangement.SpaceBetween
+	) {
+		if (onBackAction != emptyBackAction && !hideBackButton) {
+			IconButton(onClick = onBackAction) {
+				Icon(Icons.Default.ArrowBack, contentDescription = null)
+			}
+			Spacer(modifier = Modifier.width(16.dp))
+		}
+		content()
+		if (!hideOptions) OptionsRow(options)
+	}
+}
+
+@ExperimentalPagerApi
+@Composable
+fun TabbedTopAppBar(
+    pagerState: PagerState,
+    tabs: List<AppBarTab>,
+    content: @Composable () -> Unit
+) {
+
+	Surface(
+		color = MaterialTheme.colors.primarySurface,
+		contentColor = contentColorFor(MaterialTheme.colors.primarySurface),
+		elevation = AppBarDefaults.TopAppBarElevation,
+		shape = RectangleShape
+	) {
+		Column(
+			modifier = Modifier
+				.fillMaxWidth()
+		) {
+			Column(
+				modifier = Modifier
+					.fillMaxWidth()
+					.padding(AppBarDefaults.ContentPadding),
+			) {
+				content()
+			}
+			AppBarTabRow(pagerState, tabs)
+		}
+	}
+}
+
+
+@ExperimentalPagerApi
+@Composable
+fun AppBarTabRow(
+    pagerState: PagerState,
+    tabs: List<AppBarTab>,
+    modifier: Modifier = Modifier,
+    isScrollable: Boolean = false
+) {
+	val coroutineScope = rememberCoroutineScope()
+	@Composable
+	fun tabRowContent() {
+		tabs.forEachIndexed { index, it ->
+			Tab(
+				text = {
+					Text(
+						text = stringResource(it.title).uppercase(Locale.getDefault()),
+						fontSize = 14.sp
+					)
+				},
+				selected = pagerState.currentPage == index,
+				onClick = {
+					coroutineScope.launch {
+						pagerState.animateScrollToPage(index)
+					}
+				},
+				unselectedContentColor = AppColors.greyText.collectAsState().value
+			)
+		}
+	}
+	if (isScrollable)
+		ScrollableTabRow(
+			modifier = modifier.fillMaxWidth(),
+			selectedTabIndex = pagerState.currentPage,
+			indicator = { tabPositions ->
+				TabRowDefaults.Indicator(
+					Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+				)
+			},
+			contentColor = AppColors.accent.collectAsState().value
+		) {
+			tabRowContent()
+		}
+	else
+		TabRow(
+			modifier = modifier,
+			selectedTabIndex = pagerState.currentPage,
+			indicator = { tabPositions ->
+				TabRowDefaults.Indicator(
+					Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+				)
+			},
+			contentColor = AppColors.accent.collectAsState().value
+		) {
+			tabRowContent()
+		}
+}
+
+@Composable
+fun OptionsRow(
+    options: List<AppBarOption>,
+    modifier: Modifier = Modifier
+) {
+	Row(
+		modifier = modifier,
+		horizontalArrangement = Arrangement.End,
+		verticalAlignment = Alignment.CenterVertically,
+	) {
+		options.forEach { option ->
+			when (option) {
+				is AppBarOption.Clickable -> {
+					IconButton(
+						modifier = Modifier
+							.height(36.dp)
+							.width(36.dp),
+						onClick = option.onClick
+					) {
+						BadgedBox(
+							badge = {
+								if (option.badgeCount > 0)
+									Badge(
+										backgroundColor = AppColors.accent.collectAsState().value,
+										contentColor = Color.White
+									) {
+										Text(option.badgeCount.toString())
+									}
+							}
+						) {
+							Icon(
+								imageVector = option.icon,
+								contentDescription = option.contentDescription,
+								tint = MaterialTheme.colors.onSurface
+							)
+						}
+
+					}
+				}
+				is AppBarOption.Dropdown -> {
+					AppDropdownMenu(
+						anchor = {
+							IconButton(
+								modifier = Modifier
+									.height(36.dp)
+									.width(36.dp),
+								onClick = it
+							) {
+								Icon(
+									imageVector = option.icon,
+									contentDescription = option.contentDescription,
+									tint = MaterialTheme.colors.onSurface
+								)
+							}
+						},
+						content = {
+							option.dropdownMenuContent(it)
+						}
+					)
+				}
+			}
+
+		}
+	}
+}
+
+sealed class AppBarOption(
+	open val icon: ImageVector,
+	open val contentDescription: String? = null
+) {
+	class Clickable(
+		override val icon: ImageVector,
+		override val contentDescription: String? = null,
+		val badgeCount: Int = 0,
+		val onClick: () -> Unit
+	) : AppBarOption(icon, contentDescription)
+
+	class Dropdown(
+		override val icon: ImageVector,
+		override val contentDescription: String? = null,
+		val dropdownMenuContent: @Composable (collapseAction: () -> Unit) -> Unit
+	) : AppBarOption(icon, contentDescription)
+}
+
+val emptyBackAction: () -> Unit = {}
